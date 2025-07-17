acebcb2d5204c726a032d9cd384d82eba25c7507
David Wheatley <david@davwheat.dev>
2025-07-17 19:30:04 +0100

    chore: update config to serve static files

--- api/worker.ts
+++ api/worker.ts
@@ -1,15 +1,13 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run "npm run dev" in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run "npm run deploy" to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
   async fetch(request, env, ctx) {
-    return new Response('Hello World!');
+    const url = new URL(request.url)
+
+    if (url.pathname.startsWith('/api/')) {
+      return new Response(JSON.stringify({ name: 'Cloudflare' }), {
+        headers: { 'Content-Type': 'application/json' },
+      })
+    }
+
+    return env.ASSETS.fetch(request)
   },
-};
+}
